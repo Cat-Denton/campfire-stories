f1291cc9cce7f417da0c1a3e287c91b25c9acafb
Cat-Denton <willwdenton@gmail.com>
2021-09-24 12:55:26 -0700

    move header elements into a header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './Header'
 
 function App(){
   return(
     <React.Fragment>
-      <h1>Campfire Stories</h1>
+      <Header />
     </React.Fragment>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+    <h1>Campfire Stories</h1>
+  )
+}
+
+export default Header;
