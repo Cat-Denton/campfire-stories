5aad54df58e40f74887a4e20b07b8b96a22456b0
Cat-Denton <willwdenton@gmail.com>
2021-10-10 03:23:59 -0700

    add user instructions to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ function Header() {
     <h1>Campfire Stories
       <br />
       <img src={campfireLogo} alt='Stay a while and listen' />
+      <p>(click on a story to read it and write the next chapter)</p>
     </h1>
   )
 }
