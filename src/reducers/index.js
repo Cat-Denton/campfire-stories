41c0692ab96c78898013a4f48eb0435566671a19
Cat-Denton <willwdenton@gmail.com>
2021-10-09 23:52:02 -0700

    clean up reducers and tests to remove functionality that was moved to firestore

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import selectedStoryReducer from "./selected-story-reducer";
-import storyListReducer from "./story-list-reducer";
 import { combineReducers } from "redux";
 import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
   selectedStory: selectedStoryReducer,
-  masterStoryList: storyListReducer,
   firestore: firestoreReducer
 });
 
